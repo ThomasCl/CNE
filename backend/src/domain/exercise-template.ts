ca806dd4ba4cd90993de69bb8dc350d52a712054
ThomasCl <56159161+ThomasCl@users.noreply.github.com>
2023-12-16 13:01:52 +0100

    exercise templates BUT WITH CACHE

--- backend/src/domain/exercise-template.ts
+++ backend/src/domain/exercise-template.ts
@@ -6,4 +6,11 @@ export class Exercise_template {
       throw CustomError.invalid("Name or group are invalid.");
     }
   }
+
+  static reviver = (key: any, value: any) => {
+    if (typeof value === 'object' && value !== null && value._type === 'Exercise_template') {
+      return new Exercise_template(value.name, value.group); // Assuming ExerciseTemplate has a constructor that accepts an object
+    }
+    return value;
+  };
 }

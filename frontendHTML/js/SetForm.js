255ea71863c1bb998e234f088852e1625b03a0f0
Klaas <KlaasNe@users.noreply.github.com>
2023-12-18 17:21:58 +0100

    update set?

--- frontendHTML/js/SetForm.js
+++ frontendHTML/js/SetForm.js
@@ -9,13 +9,21 @@ function handleCreateSet() {
     return;
   }
 
+  if (setExists(selectedExercise.id, setNumber)) {
+    updateSet(setNumber, setWeight, setReps);
+  } else {
+    createSet(setNumber, setWeight, setReps);
+  }
+}
+
+function createSet(setNumber, setWeight, setReps) {
   // Send a POST request to create a new set
   fetch(`${host}/sets`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ exerciseId: selectedExercise.id, number:setNumber, weight:setWeight, reps:setReps }),
+    body: JSON.stringify({ exerciseId: selectedExercise.id, number: setNumber, weight: setWeight, reps: setReps }),
   })
     .then(response => response.json())
     .then(data => {
@@ -25,6 +33,36 @@ function handleCreateSet() {
     .catch(error => console.error('Error creating set:', error));
 }
 
+function updateSet(setNumber, setWeight, setReps) {
+  // Send a PUT request to create a new set
+  fetch(`${host}/exercises/${selectedExercise.id}/set/${setNumber}/edit`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ weight: setWeight, reps: setReps }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Set updated:', data);
+      renderSetList(selectedExercise.id);
+    })
+    .catch(error => console.error('Error updating set:', error));
+}
+
+async function setExists(exerciseId, setNumber) {
+  return await fetch(`${host}/sets/${exerciseId}/${setNumber}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      return data != null;
+    })
+}
+
 function handleDeleteSet(setId) {
   // Replace `setId` with the appropriate identifier for the set
 
@@ -32,13 +70,13 @@ function handleDeleteSet(setId) {
   fetch(`${host}/sets/${setId}`, {
     method: 'DELETE'
   })
-      .then(response => {
-        if (response.ok) {
-          console.log('Set deleted');
-          renderSetList(selectedExercise.id); // Refresh the set list
-        } else {
-          console.error('Error deleting set');
-        }
-      })
-      .catch(error => console.error('Error:', error));
+    .then(response => {
+      if (response.ok) {
+        console.log('Set deleted');
+        renderSetList(selectedExercise.id); // Refresh the set list
+      } else {
+        console.error('Error deleting set');
+      }
+    })
+    .catch(error => console.error('Error:', error));
 }

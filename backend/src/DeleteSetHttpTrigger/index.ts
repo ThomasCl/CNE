1168993b7f30670e6018476dadc5bd5bde594ca1
Benjamin Joossens <benjamin.joossens@student.ucll.be>
2023-12-18 13:38:53 +0100

    blabla

---
+++ backend/src/DeleteSetHttpTrigger/index.ts
@@ -0,0 +1,28 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { CustomError } from "../domain/custom-error";
+import { SetService } from "../service/set-service";
+import { openRouteWrapper } from "../helpers/function-wrapper";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    await openRouteWrapper(async () => {
+        context.log('HTTP trigger function processed a delete request.');
+
+        const setId = context.bindingData.setId; // Retrieve setId from the URL parameter
+
+        if (!setId) {
+            throw CustomError.invalid("Set ID is required.");
+        }
+
+        await SetService.getInstance().deleteSet(setId);
+
+        context.res = {
+            status: 204, // 204 No Content is appropriate for successful DELETE requests
+            body: {},
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    }, context);
+};
+
+export default httpTrigger;
--- frontendHTML/js/globals.js
+++ frontendHTML/js/globals.js
@@ -1,1 +1,2 @@
-const host = "https://workoutfunctions.azurewebsites.net/api";
+// const host = "https://workoutfunctions.azurewebsites.net/api";
+const host = "http://localhost:8080";
